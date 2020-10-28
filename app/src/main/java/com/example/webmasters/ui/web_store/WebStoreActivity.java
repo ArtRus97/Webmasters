3baab520669f2916fc6c7da17340d9cbc06ea962
Arttu Rusanen <t6ruar00@students.oamk.fi>
2020-10-28 17:04:44 +0200

    Webstore Progress

--- app/src/main/java/com/example/webmasters/ui/web_store/WebStoreActivity.java
+++ app/src/main/java/com/example/webmasters/ui/web_store/WebStoreActivity.java
@@ -3,8 +3,11 @@ package com.example.webmasters.ui.web_store;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
 
 import com.example.webmasters.R;
+import com.example.webmasters.ui.WebStoreSingleton;
 
 public class WebStoreActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class WebStoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_store);
+
+        TextView desc = findViewById(R.id.textViewDesc);
+        WebStoreSingleton singleton = WebStoreSingleton.Singleton();
+        desc.setText(singleton.desc);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.webstore_menu, menu);
+        return true;
     }
 }
