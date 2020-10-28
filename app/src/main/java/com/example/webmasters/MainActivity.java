7d4750cc104c2f847edc4f6ebd876846f226fa7d
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-10-28 06:26:56 +0200

    Create GraphicDesignActivity from template and add navigation to it from MainActivity.

--- app/src/main/java/com/example/webmasters/MainActivity.java
+++ app/src/main/java/com/example/webmasters/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.webmasters;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TEST", "Testi");
+
+        // Fetch views from layout.
+        Button buttonGraphicDesign = (Button) findViewById(R.id.buttonGraphicDesign);
+
+        // Setup view callbacks.
+        buttonGraphicDesign.setOnClickListener(this::onNavigationClick);
+    }
+
+    private void onNavigationClick(View view) {
+        switch (view.getId()) {
+            // Navigate to graphic design activity.
+            case R.id.buttonGraphicDesign:
+                Intent intent = new Intent(this, GraphicDesignActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                Log.e("MainActivity", "Invalid navigation!");
+        }
     }
 }
