0646d41063f2648883e2716336b07a573f918525
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-10-29 12:21:50 +0200

    Implement color button and start to work on logo textsize control.

--- app/src/main/java/com/example/webmasters/controllers/graphic_design/LogoFragmentController.java
+++ app/src/main/java/com/example/webmasters/controllers/graphic_design/LogoFragmentController.java
@@ -14,32 +14,39 @@ import com.example.webmasters.ui.graphic_design.logos.LogoView;
 public class LogoFragmentController {
     private Fragment mFragment;
 
+    /**
+     * Constructor
+     * @param fragment (Fragment)
+     * @param view (Fragment)
+     */
     public LogoFragmentController(Fragment fragment, View view) {
         mFragment = fragment;
         LogoViewModel model = new ViewModelProvider(mFragment).get(LogoViewModel.class);
+
         bindLogoText(view.findViewById(R.id.editLogoText), model);
+        bindLogoTextSize(view.findViewById(R.id.editLogoTextSize), model);
         bindLogoView(view.findViewById(R.id.logoView), model);
     }
 
     /**
      * bindLogoView binds logo view to view model.
      * @param logoView (LogoView)
-     * @param logosViewModel (LogosViewModel)
+     * @param logoViewModel (LogoViewModel)
      */
-    private void bindLogoView(LogoView logoView, LogoViewModel logosViewModel) {
+    private void bindLogoView(LogoView logoView, LogoViewModel logoViewModel) {
         // Model -> View updates.
-        logosViewModel.getTextObservable().observe(mFragment, logoView::setText);
-        logosViewModel.getTextSizeObservable().observe(mFragment, logoView::setTextSize);
+        logoViewModel.getTextObservable().observe(mFragment, logoView::setText);
+        logoViewModel.getTextSizeObservable().observe(mFragment, logoView::setTextSize);
     }
 
     /**
      * bindLogoText binds logo text edit view to view model and enables bidirectional updates.
      * @param editLogoText (EditText)
-     * @param logosViewModel (LogosViewModel)
+     * @param logoViewModel (LogoViewModel)
      */
-    private void bindLogoText(EditText editLogoText, LogoViewModel logosViewModel) {
+    private void bindLogoText(EditText editLogoText, LogoViewModel logoViewModel) {
         // Model -> View update.
-        logosViewModel.getTextObservable().observe(mFragment, logoText -> {
+        logoViewModel.getTextObservable().observe(mFragment, logoText -> {
             int position = editLogoText.getSelectionEnd();
             editLogoText.setText(logoText);
             editLogoText.setSelection(position);
@@ -48,7 +55,30 @@ public class LogoFragmentController {
         editLogoText.addTextChangedListener(new TextChangedListener() {
             @Override
             public void onTextChanged(String logoText) {
-                logosViewModel.setText(logoText);
+                logoViewModel.setText(logoText);
+            }
+        });
+    }
+
+    /**
+     * bindLogoTextSize binds logo text size edit view to view model and enables bidirectional updates.
+     * @param editLogoTextSize (EditText)
+     * @param logoViewModel (LogoViewModel)
+     */
+    private void bindLogoTextSize(EditText editLogoTextSize, LogoViewModel logoViewModel) {
+        // Model -> View update
+        logoViewModel.getTextSizeObservable().observe(mFragment, logoTextSize -> {
+            int position = editLogoTextSize.getSelectionEnd();
+            editLogoTextSize.setText(logoTextSize.toString());
+        });
+        // View -> Model update.
+        editLogoTextSize.addTextChangedListener(new TextChangedListener() {
+            @Override
+            public void onTextChanged(String text) {
+                try {
+                    int textSize = Integer.parseInt(text);
+                    logoViewModel.setTextSize(textSize);
+                } catch (NumberFormatException ignored) { }
             }
         });
     }
