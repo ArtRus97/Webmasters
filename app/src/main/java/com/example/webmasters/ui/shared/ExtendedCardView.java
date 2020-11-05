589f715fa0668e8a66c5c508c19e67794513e079
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-11-05 06:02:23 +0200

    Temp commit.

--- app/src/main/java/com/example/webmasters/ui/shared/ExtendedCardView.java
+++ app/src/main/java/com/example/webmasters/ui/shared/ExtendedCardView.java
@@ -24,10 +24,30 @@ import androidx.transition.TransitionSet;
 import com.example.webmasters.R;
 import com.google.android.material.card.MaterialCardView;
 
+/**
+ * ExtendedCardView extends MaterialCardView with some additional features.
+ *
+ * @author JIkaheimo
+ * <p>
+ * Features:
+ * - Showing/hiding the child views by clicking the card title.
+ * <p>
+ * Custom attributes:
+ * - app:titleText sets the the card's title text.
+ */
 public class ExtendedCardView extends MaterialCardView {
+    // Constants.
+    final String DEFAULT_TITLE = "Card Title";
+
+    // Layouts.
     private ViewGroup mLayout;
     private ViewGroup mHeader;
     private ViewGroup mContent;
+
+    // Components.
+    private TextView mTextTitle;
+    private TextView mTextIcon;
+
 
     public ExtendedCardView(Context context) {
         this(context, null);
@@ -51,18 +71,31 @@ public class ExtendedCardView extends MaterialCardView {
         mLayout = root.findViewById(R.id.layout_main);
         mHeader = mLayout.findViewById(R.id.layout_header);
         mContent = mLayout.findViewById(R.id.layout_content);
-        TextView textTitle = mHeader.findViewById(R.id.text_header);
+        mTextTitle = mHeader.findViewById(R.id.text_header_title);
+        mTextIcon = mHeader.findViewById(R.id.text_header_icon);
 
         // Set callback listeners.
-        mHeader.setOnClickListener(header -> this.toggleContent());
+        mHeader.setOnClickListener(header -> this.toggleContent(!isContentVisible()));
 
         // Set custom attributes.
-        TypedArray customAttributes = context.obtainStyledAttributes(attrs, R.styleable.ExtendedCardView, defStyle, 0);
+        TypedArray customAttributes = context.obtainStyledAttributes(attrs, R.styleable.ExtendedCardView);
         String titleText = customAttributes.getString(R.styleable.ExtendedCardView_titleText);
-        textTitle.setText(titleText);
+        if (titleText == null) {
+            titleText = DEFAULT_TITLE;
+        }
+
+        mTextTitle.setText(titleText);
         customAttributes.recycle();
     }
 
+    /**
+     * addView has to be overridden to allow XML file to pass the defined child views
+     * to the custom view's layout.
+     *
+     * @param child  (View)
+     * @param index  (int)
+     * @param params (ViewGroup.LayoutParams)
+     */
     @Override
     public void addView(View child, int index, ViewGroup.LayoutParams params) {
         if (mContent == null)
@@ -72,9 +105,8 @@ public class ExtendedCardView extends MaterialCardView {
     }
 
 
-    public void toggleContent() {
-
-        if (isContentVisible()) {
+    public void toggleContent(boolean isVisible) {
+        if (!isVisible) {
             mContent.animate()
                     .translationY(0)
                     .alpha(0.0f)
@@ -93,6 +125,6 @@ public class ExtendedCardView extends MaterialCardView {
                     .setListener(null);
 
         }
-
+        mTextIcon.setCompoundDrawablesWithIntrinsicBounds(0, 0, !isVisible ? R.drawable.ic_baseline_expand_less_24 : R.drawable.ic_baseline_expand_more_24, 0);
     }
 }
