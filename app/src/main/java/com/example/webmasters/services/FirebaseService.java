4f967081a39515ce5b67e8f9f57c3c2dbd23eea4
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2020-11-17 11:28:06 +0200

    Fix products firestore.

--- app/src/main/java/com/example/webmasters/services/FirebaseService.java
+++ app/src/main/java/com/example/webmasters/services/FirebaseService.java
@@ -1,17 +1,26 @@
 package com.example.webmasters.services;
 
 import android.util.Log;
+import androidx.annotation.NonNull;
 import com.example.webmasters.models.graphic_design.Logo;
 import com.example.webmasters.models.graphic_design.Shape;
 import com.example.webmasters.models.graphic_design.utils.ShapeFactory;
 import com.example.webmasters.models.webstore.Product;
 import com.example.webmasters.types.ILogo;
 import com.example.webmasters.types.ShapeType;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class FirebaseService {
     private final FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
@@ -43,10 +52,16 @@ public class FirebaseService {
                 });
     }
 
-    public void getProducts(Consumer<Product[]> callback) {
-        mFirestore.collection("Products").document(getUser()).get()
-                .addOnSuccessListener(documentSnapshot -> {
-                    callback.accept(documentSnapshot.toObject(Product[].class));
-                });
+    public void getProducts(Consumer<List<Product>> callback) {
+        mFirestore.collection("Products").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                List<Product> productList = new ArrayList<>();
+                for (DocumentSnapshot document : task.getResult()) {
+                     Product product = document.toObject(Product.class);
+                     productList.add(product);
+                }
+                callback.accept(productList);
+            }
+        });
     }
 }
